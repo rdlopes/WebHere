fe6eb2643bf697cd752a0d24b3e278dd42f16644
Rui Lopes <rui.d.lopes@me.com>
2013-08-04 17:55:30 +0200

    Add SenTestingKit to test targets

--- WebHereTests/WebHereTests.h
+++ WebHereTests/WebHereTests.h
@@ -6,6 +6,7 @@
 //  Copyright (c) 2013 Rui D Lopes. All rights reserved.
 //
 
+#import <SenTestingKit/SenTestingKit.h>
 #import <Kiwi/Kiwi.h>
 #import <Nocilla/Nocilla.h>
 #import <OCLogTemplate/OCLogTemplate.h>
@@ -19,3 +20,8 @@
 #import "WHNSObject.h"
 #import "WHLoginForm.h"
 
+#	define LOGGING_ENABLED        1
+#	define LOGGING_LEVEL_TRACE        0
+#	define LOGGING_LEVEL_INFO        1
+#	define LOGGING_LEVEL_ERROR        1
+#	define LOGGING_LEVEL_DEBUG        1
