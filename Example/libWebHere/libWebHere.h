f5c6c1ad13f0ad5f104b44b02ad23e413a6f4ac4
Rui Lopes <rui.d.lopes@me.com>
2014-12-29 01:16:33 +0100

    Cahnged travis config

---
+++ Example/libWebHere/libWebHere.h
@@ -0,0 +1,13 @@
+//
+//  libWebHere.h
+//  libWebHere
+//
+//  Created by Rui Lopes on 29/12/2014.
+//  Copyright (c) 2014 Rui Lopes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libWebHere : NSObject
+
+@end
