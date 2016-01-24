bad5890bb322def4352d0c550cebe40bd809b71f
Rui Lopes <rui.d.lopes@me.com>
2016-01-24 19:23:04 +0100

    Fix travis build

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-xctool -workspace WebHere.xcworkspace -scheme WebHere -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+KEY_CHAIN=ios-build.keychain
+security create-keychain -p travis $KEY_CHAIN
+# Make the keychain the default so identities are found
+security default-keychain -s $KEY_CHAIN
+# Unlock the keychain
+security unlock-keychain -p travis $KEY_CHAIN
+# Set keychain locking timeout to 3600 seconds
+security set-keychain-settings -t 3600 -u $KEY_CHAIN
+
 
